4c776b73966933e46cd60d66891031b4ae6324da
TerraFerra <deep.igorek@gmail.com>
2022-07-07 23:54:23 +0300

    Initial

---
+++ main.py
@@ -0,0 +1,43 @@
+import requests
+import json
+from flask import Flask
+
+
+def get_valutes_list():
+    url = 'https://www.cbr-xml-daily.ru/daily_json.js'
+    response = requests.get(url)
+    data = json.loads(response.text)
+    valutes = list(data['Valute'].values())
+    return valutes
+
+
+app = Flask(__name__)
+
+
+def create_html(valutes):
+    text = '<h1>Курс валют, слегка изменено и запущено Игорем</h1>'
+    text += '<table>'
+    text += '<tr>'
+    for _ in valutes[0]:
+        text += f'<th><th>'
+    text += '</tr>'
+    for valute in valutes:
+        text += '<tr>'
+        my_values = [list(valute.values())[i] for i in range(2, len(list(valute.values())))]
+        for v in my_values:
+            text += f'<td>{v}</td>'
+        text += '</tr>'
+
+    text += '</table>'
+    return text
+
+
+@app.route("/")
+def index():
+    valutes = get_valutes_list()
+    html = create_html(valutes)
+    return html
+
+
+if __name__ == "__main__":
+    app.run()
